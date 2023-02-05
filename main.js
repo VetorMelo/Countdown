009e6cf1f982b681e13e31d609df7444bfdd8875
Victor Melo <contatomelo2@gmail.com>
2023-02-05 20:45:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 'use strict';
 
-const contagemRegressiva = (tempo) => {
-    const contar = () => {
-        console.log (tempo);
-        tempo--;
-    }
-    setInterval(contar,1000);
+const formatarDigito = (digito) => `0${digito}`.slice(-2);
+
+const atualizar = (tempo) => {
+    const segundos = document.getElementById('segundos');
+    const minutos = document.getElementById('minutos');
+    const horas = document.getElementById('horas');
+
+    const qtdSegundos = tempo % 60;
+    const qtdMinutos = Math.floor((tempo % (60 * 60)) / 60);
+
+    segundos.textContent = formatarDigito(qtdSegundos);
+    minutos.textContent = formatarDigito(qtdMinutos);
 }
 
-contagemRegressiva(5);
+const contagemRegressiva = (tempo) => {
+    const pararContagem = () => clearInterval(id);
+
+    const contar = () => {
+        if (tempo == 0) {
+            pararContagem();
+        }
+        atualizar (tempo);
+        tempo--;
+    }
+    const id = setInterval(contar,1000);
+}
+
+contagemRegressiva(92);
